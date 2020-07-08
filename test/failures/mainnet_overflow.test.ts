c743bbb4f16a60b4c9305abcac10932d89784e87
Siegfried Puchbauer <siegfried.puchbauer@gmail.com>
2020-07-08 11:51:40 -0700

    fix: 🐛 avoid bigint overflow when parsing gas related info
    Changed output for gas related attributes (gas price, gas used) to be a
    number or a string instead of forcing conversion to numbers since this
    value can overflow the range of possible integers in javascript
    
    ✅ Closes: #53

---
+++ test/failures/mainnet_overflow.test.ts
@@ -0,0 +1,64 @@
+import { join } from 'path';
+import { ContractInfo } from '../../src/abi/contract';
+import { AbiRepository } from '../../src/abi/repo';
+import { BlockWatcher } from '../../src/blockwatcher';
+import { Checkpoint } from '../../src/checkpoint';
+import { BatchedEthereumClient } from '../../src/eth/client';
+import { HttpTransport } from '../../src/eth/http';
+import { withRecorder } from '../../src/eth/recorder';
+import { suppressDebugLogging } from '../../src/utils/debug';
+import { LRUCache } from '../../src/utils/lru';
+import { TestOutput } from '../testoutput';
+
+let logHandle: any;
+beforeEach(() => {
+    logHandle = suppressDebugLogging();
+});
+afterEach(() => {
+    logHandle.restore();
+});
+
+const BLOCK = 10237208;
+test(`mainnet overflow ${BLOCK}`, async () => {
+    await withRecorder(
+        new HttpTransport(`https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`, {}),
+        {
+            name: `testcases-mainnet-overflow-${BLOCK}`,
+            storageDir: join(__dirname, '../fixtures/recorded'),
+            replay: true,
+        },
+        async transport => {
+            const ethClient = new BatchedEthereumClient(transport, { maxBatchSize: 100, maxBatchTime: 0 });
+            const abiRepo = new AbiRepository({
+                decodeAnonymous: true,
+                fingerprintContracts: true,
+                abiFileExtension: '.json',
+                searchRecursive: true,
+                requireContractMatch: true,
+            });
+            await abiRepo.initialize();
+            const checkpoints = new Checkpoint({
+                initialBlockNumber: 0,
+                path: join(__dirname, `../../tmp/tmpcheckpoint_${BLOCK}.json`),
+                saveInterval: 10000,
+            });
+            const output = new TestOutput();
+            const contractInfoCache = new LRUCache<string, Promise<ContractInfo>>({ maxSize: 100 });
+            const blockWatcher = new BlockWatcher({
+                abiRepo,
+                checkpoints,
+                chunkSize: 1,
+                ethClient,
+                maxParallelChunks: 1,
+                output,
+                pollInterval: 1,
+                startAt: 'latest',
+                chunkQueueMaxSize: 10,
+                contractInfoCache,
+                waitAfterFailure: 1,
+            });
+
+            await expect(blockWatcher.processChunk({ from: BLOCK, to: BLOCK })).resolves.toBeUndefined();
+        }
+    );
+}, 15000);
